7d2b3a66a92aea0ac95630582d3b5acdae235a5f
StariyYo <forthecss@gmail.com>
2025-01-06 23:43:27 +0300

    Update jujutsu.lua

--- jujutsu.lua
+++ jujutsu.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"))()
+script_key="yourkeyhere";
+
+if  game.CreatorId == 4294803 then -- Demonfall 
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b64d07193c7c2aef970d57aeb286e70.lua"))()
+elseif game.CreatorId == 15557357 then -- Jujutsu Infinite
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4fe2dfc202115670b1813277df916ab2.lua"))()
+end
