855e6ce1c8eb8f17f7671bac79321cbd5cecc7a1
StariyYo <forthecss@gmail.com>
2025-01-07 02:50:19 +0300

    Update jujutsu.lua

--- jujutsu.lua
+++ jujutsu.lua
@@ -1,4 +1,4 @@
-script_key="yourkeyhere";
+script_key="iiADTzQROwtbQsanzekJYnCtNPnsIhcW";
 
 if  game.CreatorId == 4294803 then -- Demonfall 
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b64d07193c7c2aef970d57aeb286e70.lua"))()
